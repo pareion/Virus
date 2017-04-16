a98d1c9d8b99a9158e7db1bea2efdd47b037bec0
pareion <pareion@DESKTOP-OKHSDJS>
2017-04-17 00:11:04 +0200

    Refactored minimax so it also builds a tree

--- Virus/Virus/MiniMaxComputer.cs
+++ Virus/Virus/MiniMaxComputer.cs
@@ -16,11 +16,12 @@ namespace Virus
         int bestScoreMin;
         int bestScoreMax;
         int playerNumber;
+        Node root;
 
         public MiniMaxComputer(Board board, int playerNumber)
         {
             counter = 0;
-            maxcounter = 5;
+            maxcounter = 3;
             this.board = board;
             this.playerNumber = playerNumber;
         }
@@ -39,22 +40,22 @@ namespace Virus
                 counter = 0;
                 // find bricks you can use
                 List<Move> moves = board.FindAvailableMoves(playerNumber);
-
+                root = new Node();
                 if (moves != null)
+
                     for (int b = 0; b < moves.Count; b++)
                     {
-                        
                         tempBoard = board.Copy();
-                        
-                        minscore = MIN()+ tempBoard.MoveBrick(moves[b].fromX, moves[b].fromY, moves[b].toX, moves[b].toY); ;
+                        Node tmp = new Node();
+                        minscore = MIN(tmp) + tempBoard.MoveBrick(moves[b].fromX, moves[b].fromY, moves[b].toX, moves[b].toY);
+                        tmp.value = minscore;
+                        root.children.Add(tmp);
                         if (minscore > bestScore)
                         {
                             bestMove = moves[b];
                             bestScore = minscore;
                         }
-                        this.counter = 0;
-                        maxcounter = 0;
-
+                        counter = 0;
                     }
                 board.MoveBrick(bestMove.fromX, bestMove.fromY, bestMove.toX, bestMove.toY);
             }
@@ -64,8 +65,9 @@ namespace Virus
             }
 
         }
-        private int MIN()
+        private int MIN(Node tmp)
         {
+            counter++;
             if (isGameOverHuman())
                 return int.MinValue;
             else if (maxDepth())
@@ -78,13 +80,14 @@ namespace Virus
                 {
                     List<Move> moves = tempBoard.FindAvailableMoves(1);
 
-                    foreach (var item in moves)
+                    for (int i = 0; i < moves.Count; i++)
                     {
                         previousBoard = tempBoard.Copy();
-
-                        int score = MAX() + tempBoard.MoveBrick(item.fromX, item.fromY, item.toX, item.toY); ;
+                        Node temp = new Node();
+                        tmp.children.Add(temp);
+                        int score = MAX(tmp.children[i]) + tempBoard.MoveBrick(moves[i].fromX, moves[i].fromY, moves[i].toX, moves[i].toY);
                         beta = score;
-
+                        tmp.children[i].value = score;
                         if (beta <= alpha)
                         {
                             done = true;
@@ -95,13 +98,15 @@ namespace Virus
                             bestScoreMin = score;
                         }
                         tempBoard = previousBoard.Copy();
+                        counter--;
                     }
                 }
                 return bestScoreMin;
             }
         }
-        private int MAX()
+        private int MAX(Node tmp)
         {
+            counter++;
             if (isGameOverComputer())
                 return int.MaxValue;
             else if (maxDepth())
@@ -116,13 +121,14 @@ namespace Virus
                 {
                     List<Move> moves = tempBoard.FindAvailableMoves(2);
 
-                    foreach (var item in moves)
+                    for (int i = 0; i < moves.Count; i++)
                     {
                         previousBoard = tempBoard.Copy();
-
-                        int score = MIN() + tempBoard.MoveBrick(item.fromX, item.fromY, item.toX, item.toY);
+                        Node temp = new Node();
+                        tmp.children.Add(temp);
+                        int score = MIN(tmp.children[i]) + tempBoard.MoveBrick(moves[i].fromX, moves[i].fromY, moves[i].toX, moves[i].toY);
                         alpha = score;
-
+                        tmp.children[i].value = score;
                         if (beta <= alpha)
                         {
                             done = true;
@@ -133,7 +139,9 @@ namespace Virus
                             bestScoreMax = score;
                         }
                         tempBoard = previousBoard.Copy();
+                        counter--;
                     }
+
                 }
                 return bestScoreMin;
             }
@@ -156,16 +164,22 @@ namespace Virus
                     }
                 }
             }
+
+
             return points;
         }
 
         private bool maxDepth()
         {
             if (counter >= maxcounter)
+            {
                 return true;
+            }
             else
-                counter++;
-            return false;
+            {
+                return false;
+            }
+
         }
 
         private bool isGameOverHuman()
@@ -183,6 +197,11 @@ namespace Virus
                 return false;
             }
             return true;
+        }
+        private class Node
+        {
+            public List<Node> children = new List<Node>();
+            public int value = 0;
         }
     }
 }
